81c687b546fd0cc265199560f0fe2657e58df428
arch_laptop <chandanyuva1511@gmail.com>
2026-01-15 19:45:45 +0530

    moved colorizer to small-ones

--- nvim/.config/nvim/lua/plugins/small-ones.lua
+++ nvim/.config/nvim/lua/plugins/small-ones.lua
@@ -30,4 +30,11 @@ return {
 		event = "InsertEnter",
 		opts = {},
 	},
+	{
+		"norcalli/nvim-colorizer.lua",
+		name = "colorizer",
+		config = function()
+			require("colorizer").setup()
+		end,
+	},
 }
